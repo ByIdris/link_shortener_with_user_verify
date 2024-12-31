b28c98b340d7b3e909e615406152920f8da47202
IdrisKalay <idris.kalay@arnfintech.com>
2024-12-31 16:45:18 +0300

    Refactor ShortUrlRepository to extend Repository instead of JpaRepository

--- src/main/java/com/krh/linkshortener/infrastructure/contracts/ShortUrlRepository.java
+++ src/main/java/com/krh/linkshortener/infrastructure/contracts/ShortUrlRepository.java
@@ -2,10 +2,11 @@ package com.krh.linkshortener.infrastructure.contracts;
 
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
 import com.krh.linkshortener.domain.entities.ShortUrl;
 
-public interface ShortUrlRepository extends JpaRepository<ShortUrl, UUID> {
+public interface ShortUrlRepository extends Repository<ShortUrl, UUID> {
 
 }
